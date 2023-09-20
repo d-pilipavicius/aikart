2da72f551630fa568e17583af8fe40762e2720d3
K-Karalius <kristupaskaralius1@gmail.com>
2023-09-20 20:13:58 +0300

    added separate file for db connection to avoid leakage

--- aiKart/Program.cs
+++ aiKart/Program.cs
@@ -4,9 +4,20 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("dbconnection.json", optional: false) // Load the connectionSettings.json
+    .Build();
+
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<DataContext>(options =>
         options.UseNpgsql(connection));
+
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
