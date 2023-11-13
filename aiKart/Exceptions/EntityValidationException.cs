128d5c1e7fc913fa0d6f896ad18ca5e80fb81f5c
rokas-migauskas <37188343+rokas-migauskas@users.noreply.github.com>
2023-11-13 17:51:22 +0200

    lil fix

--- aiKart/Exceptions/EntityValidationException.cs
+++ aiKart/Exceptions/EntityValidationException.cs
@@ -1,12 +1,11 @@
 using aiKart.Interfaces;
 
 namespace aiKart.Exceptions;
-public class EntityValidationException<T> : Exception where T : IEntity
+public class EntityValidationException<T> : Exception where T : class, IEntity
 {
     public T Entity { get; }
 
-    public EntityValidationException(T entity, string message)
-        : base(message)
+    public EntityValidationException(T entity, string message) : base(message)
     {
         Entity = entity;
     }
