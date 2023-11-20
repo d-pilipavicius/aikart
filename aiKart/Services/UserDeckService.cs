3a2a46090e5f2f82cc2655dcab4c9bc94d16b158
rokas-migauskas <37188343+rokas-migauskas@users.noreply.github.com>
2023-11-20 10:55:28 +0200

    refactored UserDeckService

--- aiKart/Services/UserDeckService.cs
+++ aiKart/Services/UserDeckService.cs
@@ -1,68 +1,53 @@
-using aiKart.Data;
 using aiKart.Interfaces;
 using aiKart.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace aiKart.Services
 {
     public class UserDeckService : IUserDeckService
     {
-        private readonly DataContext _context;
+        private readonly IUserDeckRepository _userDeckRepository;
 
-        public UserDeckService(DataContext context)
+        public UserDeckService(IUserDeckRepository userDeckRepository)
         {
-            _context = context;
+            _userDeckRepository = userDeckRepository;
         }
 
         public ICollection<UserDeck> GetUserDecks()
         {
-            return _context.UserDecks.OrderBy(u => u.UserId).ToList();
+            return _userDeckRepository.GetUserDecks();
         }
 
         public ICollection<Deck?> GetDecksByUser(int userId)
         {
-            return _context.UserDecks.Where(u => u.UserId == userId).Include(o => o.Deck.Cards).Select(o => o.Deck).ToList();
+            return _userDeckRepository.GetDecksByUser(userId);
         }
 
         public ICollection<User?> GetUsersOfADeck(int deckId)
         {
-            return _context.UserDecks.Where(u => u.DeckId == deckId).Select(o => o.User).ToList();
+            return _userDeckRepository.GetUsersOfADeck(deckId);
         }
 
         public bool UserDeckExists(int userId, int deckId)
         {
-            return _context.UserDecks.Any(u => u.UserId == userId && u.DeckId == deckId);
+            return _userDeckRepository.UserDeckExists(userId, deckId);
         }
 
         public bool AddUserDeck(UserDeck userDeck)
         {
-            _context.Add(userDeck);
-            return Save();
+            return _userDeckRepository.AddUserDeck(userDeck);
         }
 
         public bool DeleteUserDeck(int userId, int deckId)
         {
-            var userDeck = _context.UserDecks.FirstOrDefault(ud => ud.UserId == userId && ud.DeckId == deckId);
+            var userDeck = _userDeckRepository.GetUserDecks().FirstOrDefault(ud => ud.UserId == userId && ud.DeckId == deckId);
             if (userDeck == null) return false;
-            _context.UserDecks.Remove(userDeck);
-            return Save();
+            return _userDeckRepository.DeleteUserDeck(userDeck);
         }
 
         public bool Save()
         {
-            try
-            {
-                return _context.SaveChanges() > 0;
-            }
-            catch (Exception ex)
-            {
-                // Log the exception (Console.WriteLine is not the best way to handle exceptions here)
-                Console.WriteLine($"An error occurred while saving changes: {ex.Message}");
-                return false;
-            }
+            return _userDeckRepository.Save();
         }
     }
 }
